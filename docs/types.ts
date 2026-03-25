d4df1eaf9acd66720891c5f39348d42113d2f1ad
gilly-lgtm <262495025+gilly-lgtm@users.noreply.github.com>
2026-03-26 01:34:10 +1000

    defined(val)

--- docs/types.ts
+++ docs/types.ts
@@ -1,7 +1,11 @@
+export type Defined<T> = T extends undefined | null ? never : T;
 
-export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+export function assertIsDefined<T>(val: T, message?: string): asserts val is Defined<T> {
   if (val === undefined || val === null) {
-    throw new Error(`Expected 'val' to be defined, but received ${val}`);
+    throw new Error(message || `Expected 'val' to be defined, but received ${String(val)}`);
   }
 }
 
+export function isDefined<T>(val: T): val is Defined<T> {
+  return val !== undefined && val !== null;
+}
